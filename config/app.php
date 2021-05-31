01dfa0e5557099c07415ec83d6ec4c63878f90ce
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-05-31 14:25:09 -0500

    estructura basica del proyecto

---
+++ config/app.php
@@ -0,0 +1,4 @@
+<?php
+  const SERVERURL = "http://localhost/cursos/mvc_prestamos_app/";
+  const COLEGIO = "CNI - Imperial";
+  date_default_timezone_set("America/Lima");
---
+++ config/server.php
@@ -0,0 +1,11 @@
+<?php
+  const SERVER = "localhost";
+  const DB = "cni_app";
+  const USER = "root";
+  const PASSWORD = "";
+
+  const SGDB = "mysql:host=" . SERVER . ";dbname=" . DB;
+
+  const METHOD = "AES-256-CBC";
+  const SECRET_KEY = '$594-+4sf-65d6&&00#4510';
+  const SECRET_IV = "96485003520";
