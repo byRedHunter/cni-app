04abb6435d600bd3570c18324bcef3f21f38061f
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-06-10 18:48:23 -0500

    clase MainModel

--- views/components/scripts.php
+++ views/components/scripts.php
@@ -1,20 +1,23 @@
+<!-- Sweet Alert V8.13.0 JS file-->
+<script src="<?php echo SERVERURL; ?>views/assets/js/sweetalert2.min.js"></script>
+
 <!--=============================================
 =            Include JavaScript files           =
 ==============================================-->
 <!-- jQuery V3.4.1 -->
-<script src="<?php echo SERVERURL; ?>views/assets/js/jquery-3.4.1.min.js" ></script>
+<script src="<?php echo SERVERURL; ?>views/assets/js/jquery-3.4.1.min.js"></script>
 
 <!-- popper -->
-<script src="<?php echo SERVERURL; ?>views/assets/js/popper.min.js" ></script>
+<script src="<?php echo SERVERURL; ?>views/assets/js/popper.min.js"></script>
 
 <!-- Bootstrap V4.3 -->
-<script src="<?php echo SERVERURL; ?>views/assets/js/bootstrap.min.js" ></script>
+<script src="<?php echo SERVERURL; ?>views/assets/js/bootstrap.min.js"></script>
 
 <!-- jQuery Custom Content Scroller V3.1.5 -->
-<script src="<?php echo SERVERURL; ?>views/assets/js/jquery.mCustomScrollbar.concat.min.js" ></script>
+<script src="<?php echo SERVERURL; ?>views/assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
 
 <!-- Bootstrap Material Design V4.0 -->
-<script src="<?php echo SERVERURL; ?>views/assets/js/bootstrap-material-design.min.js" ></script>
+<script src="<?php echo SERVERURL; ?>views/assets/js/bootstrap-material-design.min.js"></script>
 <script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
 
 <!-- landing -->
--- views/components/styles.php
+++ views/components/styles.php
@@ -10,9 +10,6 @@
 <!-- Sweet Alerts V8.13.0 CSS file -->
 <link rel="stylesheet" href="<?php echo SERVERURL; ?>views/assets/css/sweetalert2.min.css">
 
-<!-- Sweet Alert V8.13.0 JS file-->
-<script src="<?php echo SERVERURL; ?>views/assets/js/sweetalert2.min.js" ></script>
-
 <!-- jQuery Custom Content Scroller V3.1.5 -->
 <link rel="stylesheet" href="<?php echo SERVERURL; ?>views/assets/css/jquery.mCustomScrollbar.css">
 
