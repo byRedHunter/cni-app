dc92a7411eade2d9da9e3a77c7d3a4e96ed4f4f1
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-06-24 10:54:17 -0500

    base de datos

---
+++ db/cni_app.sql
@@ -0,0 +1,203 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-06-2021 a las 17:42:09
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cni_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libro`
+--
+
+CREATE TABLE `libro` (
+  `idLibro` int(11) NOT NULL,
+  `titulo` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `autor` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `serie` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `categoria` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `estado` varchar(10) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Disponible'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recepcion`
+--
+
+CREATE TABLE `recepcion` (
+  `idRecepcion` int(11) NOT NULL,
+  `asunto` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `tipoDocumento` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `archivo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `fecha` date NOT NULL,
+  `estado` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `idSolicitante` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `solicitante`
+--
+
+CREATE TABLE `solicitante` (
+  `idSolicitante` int(11) NOT NULL,
+  `dni` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `apellido` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `celular` varchar(9) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `solicitud_libro`
+--
+
+CREATE TABLE `solicitud_libro` (
+  `idSolicitudLibro` int(11) NOT NULL,
+  `tipoSolicitante` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `fechaRecojo` date NOT NULL,
+  `fechaDevolucion` date NOT NULL,
+  `idSolicitante` int(11) NOT NULL,
+  `idLibro` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `dni` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `nombre` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
+  `apellido` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `clave` text COLLATE utf8_unicode_ci NOT NULL,
+  `privilegio` int(1) NOT NULL,
+  `estado` varchar(10) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Activo'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `dni`, `nombre`, `apellido`, `username`, `email`, `clave`, `privilegio`, `estado`) VALUES
+(1, '73109572', 'Jhonny', 'Quispe Navarro', 'RedHunter', '', 'bTRKYUZSdHh5NTQvUTMwcVRjcjFNdz09', 1, 'Activo');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `libro`
+--
+ALTER TABLE `libro`
+  ADD PRIMARY KEY (`idLibro`);
+
+--
+-- Indices de la tabla `recepcion`
+--
+ALTER TABLE `recepcion`
+  ADD PRIMARY KEY (`idRecepcion`),
+  ADD KEY `fk_solicitante_recepcion` (`idSolicitante`);
+
+--
+-- Indices de la tabla `solicitante`
+--
+ALTER TABLE `solicitante`
+  ADD PRIMARY KEY (`idSolicitante`);
+
+--
+-- Indices de la tabla `solicitud_libro`
+--
+ALTER TABLE `solicitud_libro`
+  ADD PRIMARY KEY (`idSolicitudLibro`),
+  ADD KEY `fk_solicitante_solicitudLibro` (`idSolicitante`),
+  ADD KEY `fk_libro_solicitudLibro` (`idLibro`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD UNIQUE KEY `dni` (`dni`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `libro`
+--
+ALTER TABLE `libro`
+  MODIFY `idLibro` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `recepcion`
+--
+ALTER TABLE `recepcion`
+  MODIFY `idRecepcion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `solicitante`
+--
+ALTER TABLE `solicitante`
+  MODIFY `idSolicitante` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `solicitud_libro`
+--
+ALTER TABLE `solicitud_libro`
+  MODIFY `idSolicitudLibro` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `recepcion`
+--
+ALTER TABLE `recepcion`
+  ADD CONSTRAINT `fk_solicitante_recepcion` FOREIGN KEY (`idSolicitante`) REFERENCES `solicitante` (`idSolicitante`);
+
+--
+-- Filtros para la tabla `solicitud_libro`
+--
+ALTER TABLE `solicitud_libro`
+  ADD CONSTRAINT `fk_libro_solicitudLibro` FOREIGN KEY (`idLibro`) REFERENCES `libro` (`idLibro`),
+  ADD CONSTRAINT `fk_solicitante_solicitudLibro` FOREIGN KEY (`idSolicitante`) REFERENCES `solicitante` (`idSolicitante`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
