db035bf1ac7942837d5503db11bde271829aed0c
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-06-25 10:17:22 -0500

    diseño de las interfaces para la mesa de recepcion

--- views/pages/desk-list-view.php
+++ views/pages/desk-list-view.php
@@ -1,139 +1,84 @@
 <div class="full-box page-header">
-    <h3 class="text-left">
-        <i class="fas fa-clipboard-list fa-fw"></i> &nbsp; FINALIZADOS
-    </h3>
-    <p class="text-justify">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia fugiat est ducimus inventore, repellendus deserunt cum aliquam dignissimos, consequuntur molestiae perferendis quae, impedit doloribus harum necessitatibus magnam voluptatem voluptatum alias!
-    </p>
+	<h3 class="text-left">
+		<i class="fas fa-clipboard-list fa-fw"></i> &nbsp; TODOS LOS DOCUMENTOS
+	</h3>
+	<p class="text-justify">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia fugiat est ducimus inventore, repellendus deserunt cum aliquam dignissimos, consequuntur molestiae perferendis quae, impedit doloribus harum necessitatibus magnam voluptatem voluptatum alias!
+	</p>
 </div>
 
 <div class="container-fluid">
-    <ul class="full-box list-unstyled page-nav-tabs">
-        <li>
-            <a href="<?php echo SERVERURL; ?>desk-recents"><i class="far fa-calendar-alt"></i> &nbsp; Recientes</a>
-        </li>
-        <li>
-            <a href="<?php echo SERVERURL; ?>desk-list"><i class="fas fa-hand-holding-usd fa-fw"></i> &nbsp; Todos</a>
-        </li>
-        <li>
-            <a href="<?php echo SERVERURL; ?>desk-search"><i class="fas fa-search-dollar fa-fw"></i> &nbsp; BUSCAR POR FECHA</a>
-        </li>
-    </ul>
+	<ul class="full-box list-unstyled page-nav-tabs">
+		<li>
+			<a href="<?php echo SERVERURL; ?>desk-recents"><i class="far fa-calendar-alt"></i> &nbsp; Recientes</a>
+		</li>
+		<li>
+			<a class="active" href="<?php echo SERVERURL; ?>desk-list"><i class="fas fa-hand-holding-usd fa-fw"></i> &nbsp; Todos</a>
+		</li>
+		<li>
+			<a href="<?php echo SERVERURL; ?>desk-search"><i class="fas fa-search-dollar fa-fw"></i> &nbsp; BUSCAR POR FECHA</a>
+		</li>
+	</ul>
 </div>
 
  <div class="container-fluid">
 	<div class="table-responsive">
-		<table class="table table-dark table-sm">
+	<table class="table table-dark table-sm">
 			<thead>
 				<tr class="text-center roboto-medium">
 					<th>#</th>
-					<th>CLIENTE</th>
-					<th>FECHA DE PRÉSTAMO</th>
-					<th>FECHA DE ENTREGA</th>
+					<th><span class="text-table">asdfgfsdf</span>ASUNTO<span class="text-table">asdfgfsdf</span></th>
 					<th>TIPO</th>
-					<th>ESTADO</th>
-					<th>FACTURA</th>
-					<th>ACTUALIZAR</th>
-					<th>ELIMINAR</th>
+					<th>FECHA</th>
+					<th><span class="text-table">asdfgfsdf</span>SOLICITANTE<span class="text-table">asdfgfsdf</span></th>
+					<th>EMAIL</th>
+					<th>CELULAR</th>
+					<th>ESTADO</th> <!-- NUEVO - RESPODIDO -->
+					<th>DOCUMENTO</th>
+					<th>RESPONDIDO</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr class="text-center" >
 					<td>1</td>
-					<td>NOMBRE CLIENTE</td>
-					<td>2017/10/8</td>
-					<td>2017/10/10</td>
-					<td><span class="badge badge-dark">Finalizado</span></td>
-					<td><span class="badge badge-primary">Cancelado</span></td>
+					<td>Constancia de Estudios</td>
+					<td>Solicitud</td>
+					<td>30/06/2021</td>
+					<td>Mario Guerra Fernandez</td>
+					<td>mario.guerra@gmail.com</td>
+					<td>965485785</td>
+					<td><span class="badge badge-primary">Nuevo</span></td>
 					<td>
 						<a href="#" class="btn btn-info">
 								<i class="fas fa-file-pdf"></i>	
 						</a>
 					</td>
 					<td>
-						<a href="reservation-update.html" class="btn btn-success">
-								<i class="fas fa-sync-alt"></i>	
-						</a>
-					</td>
-					<td>
 						<form action="">
 							<button type="button" class="btn btn-warning">
-									<i class="far fa-trash-alt"></i>
+									<i class="fas fa-sync-alt"></i>
 							</button>
 						</form>
 					</td>
 				</tr>
 				<tr class="text-center" >
 					<td>2</td>
-					<td>NOMBRE CLIENTE</td>
-					<td>2017/10/8</td>
-					<td>2017/10/10</td>
-					<td><span class="badge badge-dark">Finalizado</span></td>
-					<td><span class="badge badge-primary">Cancelado</span></td>
+					<td>Constancia de Estudios</td>
+					<td>Solicitud</td>
+					<td>30/06/2021</td>
+					<td>Mario Guerra Fernandez</td>
+					<td>mario.guerra@gmail.com</td>
+					<td>965485785</td>
+					<td><span class="badge badge-info">Respondido</span></td>
 					<td>
 						<a href="#" class="btn btn-info">
 								<i class="fas fa-file-pdf"></i>	
 						</a>
 					</td>
 					<td>
-						<a href="reservation-update.html" class="btn btn-success">
-								<i class="fas fa-sync-alt"></i>	
-						</a>
-					</td>
-					<td>
 						<form action="">
-							<button type="button" class="btn btn-warning">
-									<i class="far fa-trash-alt"></i>
-							</button>
-						</form>
-					</td>
-				</tr>
-				<tr class="text-center" >
-					<td>3</td>
-					<td>NOMBRE CLIENTE</td>
-					<td>2017/10/8</td>
-					<td>2017/10/10</td>
-					<td><span class="badge badge-dark">Finalizado</span></td>
-					<td><span class="badge badge-primary">Cancelado</span></td>
-					<td>
-						<a href="#" class="btn btn-info">
-								<i class="fas fa-file-pdf"></i>	
-						</a>
-					</td>
-					<td>
-						<a href="reservation-update.html" class="btn btn-success">
-								<i class="fas fa-sync-alt"></i>	
-						</a>
-					</td>
-					<td>
-						<form action="">
-							<button type="button" class="btn btn-warning">
-									<i class="far fa-trash-alt"></i>
-							</button>
-						</form>
-					</td>
-				</tr>
-				<tr class="text-center" >
-					<td>4</td>
-					<td>NOMBRE CLIENTE</td>
-					<td>2017/10/8</td>
-					<td>2017/10/10</td>
-					<td><span class="badge badge-dark">Finalizado</span></td>
-					<td><span class="badge badge-primary">Cancelado</span></td>
-					<td>
-						<a href="#" class="btn btn-info">
-								<i class="fas fa-file-pdf"></i>	
-						</a>
-					</td>
-					<td>
-						<a href="reservation-update.html" class="btn btn-success">
-								<i class="fas fa-sync-alt"></i>	
-						</a>
-					</td>
-					<td>
-						<form action="">
-							<button type="button" class="btn btn-warning">
-									<i class="far fa-trash-alt"></i>
+							<button type="button" class="btn btn-warning" disabled>
+									<i class="fas fa-sync-alt"></i>
 							</button>
 						</form>
 					</td>
